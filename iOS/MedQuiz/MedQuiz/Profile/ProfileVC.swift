1f9ff8479062893dc40a006d447fca74a0e1d716
Chad Johnson <chadjohnson@Chads-Mac.local>
2018-02-14 15:16:48 -0800

    Revert changes mistakenly made to Profile Page files.

--- iOS/MedQuiz/MedQuiz/Profile/ProfileVC.swift
+++ iOS/MedQuiz/MedQuiz/Profile/ProfileVC.swift
@@ -1,3 +1,4 @@
+
 //
 //  ProfileVC.swift
 //  MedQuiz
@@ -5,7 +6,6 @@
 //  Created by Omar Sherief on 11/9/17.
 //  Copyright © 2017 Omar Sherief. All rights reserved.
 //
-
 import Foundation
 import UIKit
 
@@ -13,43 +13,23 @@ class ProfileVC: UIViewController {
     
     @IBOutlet weak var profileImage: UIImageView!
     
-    @IBOutlet weak var lastQuizRankLabel: UILabel!
-    @IBOutlet weak var globalRankLabel: UILabel!
-    @IBOutlet weak var friendsRankLabel: UILabel!
-    
-    @IBOutlet weak var usernameLabel: UILabel!
-    
-    var username = "KetSwiftMoves"
-    
-    var lastQuizRank = 4
-    var globalRank = 20
-    var friendsRank = 1
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let profileAvatarChange = UITapGestureRecognizer(target: self, action: #selector(ProfileVC.profileAvatarPressed))
+        let profileAvatarChange = UITapGestureRecognizer(target: self, action: Selector("profileAvatarPressed"))
         
         profileImage.addGestureRecognizer(profileAvatarChange)
         
-        usernameLabel.text = username
-        
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .ordinal
-        
-        lastQuizRankLabel.text = formatter.string(from: NSNumber(value: lastQuizRank))
-        globalRankLabel.text = formatter.string(from: NSNumber(value: globalRank))
-        friendsRankLabel.text = formatter.string(from: NSNumber(value: friendsRank))
     }
     
     @IBAction func addFriendsPressed(_ sender: Any) {
         let addFriendsVC =  self.storyboard?.instantiateViewController(withIdentifier: "addFriends") as! AddFriendsVC
-        self.present(addFriendsVC, animated: false, completion: nil) 
+        self.present(addFriendsVC, animated: false, completion: nil)
     }
     
     func profileAvatarPressed(){
         print("woo")
-       let changeAvatarVC = self.storyboard?.instantiateViewController(withIdentifier: "changeAvatar") as! ChangeAvatarVC
+        let changeAvatarVC = self.storyboard?.instantiateViewController(withIdentifier: "changeAvatar") as! ChangeAvatarVC
         self.present(changeAvatarVC, animated: false, completion: nil)
     }
     
