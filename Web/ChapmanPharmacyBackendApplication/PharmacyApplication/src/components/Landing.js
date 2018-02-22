571e48c121d5d6a6e964fe959eeb7e1c7455f7c2
shayn <zamor122@mail.chapman.edu>
2018-02-21 19:27:25 -0800

    Resets all code for sign up and other pages to make more generic
    Removes all the Vendyr information

--- Web/ChapmanPharmacyBackendApplication/PharmacyApplication/src/components/Landing.js
+++ Web/ChapmanPharmacyBackendApplication/PharmacyApplication/src/components/Landing.js
@@ -3,6 +3,7 @@ import React from 'react';
 const LandingPage = () =>
     <div>
         <h1>Landing Page</h1>
+        <p>Enter the main landing page content between the two divs!!! :)</p>
     </div>
 
 export default LandingPage;
