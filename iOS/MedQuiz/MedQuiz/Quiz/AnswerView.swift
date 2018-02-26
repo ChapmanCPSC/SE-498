67137704ea1570a8753f75688031272be7010cda
Paul Harnack <harna100@mail.chapman.edu>
2018-02-25 22:38:17 -0800

    Replaces old views in quiz activity with new AnswerViews

--- iOS/MedQuiz/MedQuiz/Quiz/AnswerView.swift
+++ iOS/MedQuiz/MedQuiz/Quiz/AnswerView.swift
@@ -15,8 +15,12 @@ class AnswerView: UIView {
     @IBOutlet weak var iv_wrong: UIImageView!
     @IBOutlet weak var lab_answerText: UILabel!
     @IBOutlet weak var lab_points: UILabel!
-    
     @IBOutlet var viewMain: UIView!
+    
+    @IBOutlet weak var con_imgAnswerHeight: NSLayoutConstraint!
+    
+    var answer:AnswerModel!
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -29,7 +33,105 @@ class AnswerView: UIView {
     
     func setupView(){
         Bundle.main.loadNibNamed("AnswerView", owner: self, options: nil)
+        addSubviews()
+    }
+    
+    func addSubviews(){
+        addSubview(viewMain)
+        viewMain.frame = self.bounds
+        viewMain.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        viewMain.addSubview(iv_answer)
+        viewMain.addSubview(iv_wrong)
+        viewMain.addSubview(iv_correct)
+        viewMain.addSubview(lab_answerText)
+        viewMain.addSubview(lab_points)
+
+    }
+
+    func setAnswer(answer:AnswerModel) {
+        self.answer = answer
+        resetViews()
+    }
+    
+    func showCorrect() {
+        iv_correct.isHidden = false
+    }
+    
+    func showWrong() {
+        iv_wrong.isHidden = false
+    }
+    
+    func resetViews() {
+        if (answer.hasImage){
+            showImage()
+            hideText()
+        }
+        else{
+            hideImage()
+            showText()
+        }
+    }
+    
+    func hideImage() {
+        let newConstraint = con_imgAnswerHeight.constraintWithMultipler(0.0000000000000001)
+        viewMain.removeConstraint(con_imgAnswerHeight)
+        con_imgAnswerHeight = newConstraint
+        viewMain.addConstraint(con_imgAnswerHeight)
+        viewMain.layoutIfNeeded()
+//        lab_answerText.layoutIfNeeded()
+        showText()
+    }
+    
+    func hideText() {
+        lab_answerText.isHidden = true
+
+
+
+        showImage()
+    }
+    
+    private func showImage() {
+        let newConstraint = con_imgAnswerHeight.constraintWithMultipler(0.5)
+        viewMain.removeConstraint(con_imgAnswerHeight)
+        con_imgAnswerHeight = newConstraint
+        viewMain.addConstraint(con_imgAnswerHeight)
+        viewMain.layoutIfNeeded()
+    }
+    
+    private func showText() {
+        lab_answerText.isHidden = false
+    }
+    
+    func showPoints() {
+        
+    }
+    
+    func hidePoints() {
         
     }
 
+    func setBackgroundColor(color:String) {
+        viewMain.backgroundColor = UIColor.hexStringToUIColor(hex: color)
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- iOS/MedQuiz/MedQuiz/Quiz/QuizActivityVC.swift
+++ iOS/MedQuiz/MedQuiz/Quiz/QuizActivityVC.swift
@@ -13,18 +13,22 @@ class QuizActivityVC: UIViewController {
     
     var currQuestion:QuestionModel!
     
-    @IBOutlet weak var answer1: UIView!
-    @IBOutlet weak var answer2: UIView!
-    @IBOutlet weak var answer3: UIView!
-    @IBOutlet weak var answer4: UIView!
-    var answerViews:[UIView]!
+    @IBOutlet weak var answer1: AnswerView!
+    @IBOutlet weak var answer2: AnswerView!
+    @IBOutlet weak var answer3: AnswerView!
+    @IBOutlet weak var answer4: AnswerView!
+    var answerViews:[AnswerView]!
+    var colors:[String] = ["#BB7AE1", "#DCA480", "#DA7E7E", "#88D3E5"]
 
+    var toggleTemp:Bool = false
     @IBOutlet weak var lab_questionText: UILabel!
     @IBOutlet weak var lab_questionNumber: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         answerViews = [answer1, answer2, answer3, answer4]
+        answerViews.forEach { view in view.hideImage() }
+        setAnswerColors()
     }
     
     func registerFirebaseListeners(){
@@ -34,13 +38,21 @@ class QuizActivityVC: UIViewController {
     func sendIsReady(){
         // tell firebase that this client is ready for next question
     }
-    
+
+    func setAnswerColors(){
+        var count = 0
+        answerViews.forEach { view in
+            view.setBackgroundColor(color: colors[count])
+            count += 1
+        }
+    }
+
     func reloadView(){
         // upon getting a new question update the view
         updateQuestionNumber(text: "1")
         updateQuestionText(text: "What is the question from swift")
         updateAnswerTexts(texts: ["Answer1", "Answer2", "Answer3", "Answer4"])
-        // updateAnswerPictures(urls: [String])
+//         updateAnswerPictures(urls: [String])
         
     }
     
@@ -60,5 +72,14 @@ class QuizActivityVC: UIViewController {
         
     }
     
+    @IBAction func tempPressed(_ sender: Any) {
+        if(toggleTemp){
+            answerViews.forEach { view in view.hideText() }
+        }
+        else{
+            answerViews.forEach { view in view.hideImage() }
+        }
+        toggleTemp = !toggleTemp
+    }
 }
 
