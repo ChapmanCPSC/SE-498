6fab1cc35db3790c9e0019ba34abbeafacb4f4c7
Paul Harnack <harna100@mail.chapman.edu>
2018-02-26 18:45:12 -0800

    Fills out quiz model, matching schema from web team's github document

--- iOS/MedQuiz/MedQuiz/Quiz/QuizModel.swift
+++ iOS/MedQuiz/MedQuiz/Quiz/QuizModel.swift
@@ -8,5 +8,23 @@
 
 import Foundation
 class QuizModel {
-    
+    var dateCreated:Date
+    var available:Bool
+    var visible:Bool
+    var classBelongsTo:String
+    var points: Bool
+    var title:String
+    var questions:[QuestionModel]
+    var tags:[String] // TODO create a TagModel
+
+    init(dateCreated:Date, available:Bool, visible:Bool, classBelongsTo:String, points: Bool, title:String, questions:[QuestionModel], tags:[String]){
+        self.dateCreated = dateCreated
+        self.available = available
+        self.visible = visible
+        self.classBelongsTo = classBelongsTo
+        self.points = points
+        self.title = title
+        self.questions = questions
+        self.tags = tags
+    }
 }
