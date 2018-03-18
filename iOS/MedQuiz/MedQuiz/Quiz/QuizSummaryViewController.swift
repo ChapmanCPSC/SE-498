61854f6e537a5e18630303a26d9bdabaa29307cf
Paul Harnack <harna100@mail.chapman.edu>
2018-03-17 23:46:04 -0700

    Adds view controller for quiz summary, adds outlets from quiz summary into vc, adds methods for setting the labels

---
+++ iOS/MedQuiz/MedQuiz/Quiz/QuizSummaryViewController.swift
@@ -0,0 +1,84 @@
+//
+//  QuizSummaryViewController.swift
+//  MedQuiz
+//
+//  Created by Harnack, Paul (Student) on 3/17/18.
+//  Copyright © 2018 Omar Sherief. All rights reserved.
+//
+
+import UIKit
+
+class QuizSummaryViewController: UIViewController {
+
+    @IBOutlet weak var iv_profilePic: UIImageView!
+    @IBOutlet weak var lab_username: UILabel!
+    @IBOutlet weak var lab_score: UILabel!
+    @IBOutlet weak var lab_rank: UILabel!
+    @IBOutlet weak var lab_earnedPoints: UILabel!
+    @IBOutlet weak var lab_scoreDiff: UILabel!
+    @IBOutlet weak var lab_questionsRight: UILabel!
+    @IBOutlet weak var lab_questionsWrong: UILabel!
+    
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setTempValues()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btn_donePressed(_ sender: Any) {
+
+    }
+    
+    func setRankLabel(position:Int){
+        let rank = String.ordinalNumberFormat(number: position)
+        lab_rank.text = "You ranked \(rank)"
+    }
+
+    func setEarnedPointsLabel(points:Int){
+        let earnedPoints = points > 0 ? "+\(points)" : "\(points)"
+        lab_earnedPoints.text = "Earned \(earnedPoints) points"
+    }
+
+    func setScoreDiffLabel(startPoints:Int, endPoints:Int){
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        let formattedStart = formatter.string(from: NSNumber(value: startPoints))
+        let formattedEnd = formatter.string(from: NSNumber(value: endPoints))
+        let changeWord = endPoints - startPoints > 0 ? "up" : "down"
+
+        lab_scoreDiff.text = "Score went \(changeWord) from \(formattedStart!) to \(formattedEnd!)"
+    }
+
+    func setQuestionsRightLabel(questionsRight:Int){
+        lab_questionsRight.text = "Got \(questionsRight) questions right"
+    }
+
+    func setQuestionsWrongLabel(questionsWrong:Int){
+        lab_questionsWrong.text = "Got \(questionsWrong) questions wrong"
+    }
+
+    func setTotalPointsLabel(totalPoints:Int){
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        let formattedScore = formatter.string(from: NSNumber(value:totalPoints))
+        lab_score.text = formattedScore
+    }
+
+
+    func setTempValues(){
+        setRankLabel(position: 5)
+        setEarnedPointsLabel(points: -400)
+        setScoreDiffLabel(startPoints: 900, endPoints: 500)
+        setQuestionsRightLabel(questionsRight: 10)
+        setQuestionsWrongLabel(questionsWrong: 2)
+        setTotalPointsLabel(totalPoints: 900)
+    }
+}
