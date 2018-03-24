a9198cfba1ad982996b30c65e8b94aeb3c3a4bac
Omar Sherief <omarsherief_108@Hotmail.com>
2018-03-23 20:00:00 -0700

    Working example of using OurColorHelper. And some changes to ColorHelper class

--- iOS/MedQuiz/MedQuiz/ColorHelper.swift
+++ iOS/MedQuiz/MedQuiz/ColorHelper.swift
@@ -16,40 +16,39 @@ import UIKit
 
 
 //use this class helper instance throught app to help with colors
-var OurColorHelper = colorHelper()
-
-//Colors
-//let leaderBoardSwitchFriends = OurColorHelper.hexStringToUIColor(hex: "6FE5CB")
-//let leaderBoardSwitchGlobal = OurColorHelper.hexStringToUIColor(hex: "E5896F")
-//let leaderBoardHiglightUser = OurColorHelper.hexStringToUIColor(hex: "FFE483")
-
-let pharmAppTeal = OurColorHelper.hexStringToUIColor(hex: "6FE5CB")
-let pharmAppDarkTeal = OurColorHelper.hexStringToUIColor(hex: "9AD5D2")
-let pharmAppBlue = OurColorHelper.hexStringToUIColor(hex: "439EC4")
-let pharmAppDarkBlue = OurColorHelper.hexStringToUIColor(hex: "30718D")
-let pharmAppYellow = OurColorHelper.hexStringToUIColor(hex: "FFE483")
-let pharmAppOrange = OurColorHelper.hexStringToUIColor(hex: "FFC484")
-let pharmAppPurple = OurColorHelper.hexStringToUIColor(hex: "8884FF")
-let pharmAppRed = OurColorHelper.hexStringToUIColor(hex: "FF8D84")
-let pharmAppGrey = OurColorHelper.hexStringToUIColor(hex: "979797")
-
-let pharmAppAnswerOne = OurColorHelper.hexStringToUIColor(hex: "BB7AE1")
-let pharmAppAnswerTwo = OurColorHelper.hexStringToUIColor(hex: "DCA480")
-let pharmAppAnswerThree = OurColorHelper.hexStringToUIColor(hex: "DA7E7E")
-let pharmAppAnswerFour = OurColorHelper.hexStringToUIColor(hex: "88D3E5")
-
-
-let pharmAppGameLeaderboardUserOne = OurColorHelper.hexStringToUIColor(hex: "98D8C0")
-let pharmAppGameLeaderboardUserTwo = OurColorHelper.hexStringToUIColor(hex: "C892D8")
-let pharmAppGameLeaderboardCurrentUser = OurColorHelper.hexStringToUIColor(hex: "FDFFA1")
-let pharmAppGameLeaderboardUserThree = OurColorHelper.hexStringToUIColor(hex: "A1ABFF")
-let pharmAppGameLeaderboardUserFour = OurColorHelper.hexStringToUIColor(hex: "D8939B")
+var OurColorHelper = ColorHelper()
 
 
 
 //Color helper class
-class colorHelper{
+class ColorHelper{
     
+    //Colors
+    //lazy to allow variables to use method in self
+    lazy var pharmAppTeal = hexStringToUIColor(hex: "6FE5CB")
+    lazy var pharmAppDarkTeal = hexStringToUIColor(hex: "9AD5D2")
+    lazy var pharmAppBlue = hexStringToUIColor(hex: "439EC4")
+    lazy var pharmAppDarkBlue = hexStringToUIColor(hex: "30718D")
+    lazy var pharmAppYellow = hexStringToUIColor(hex: "FFE483")
+    lazy var pharmAppOrange = hexStringToUIColor(hex: "FFC484")
+    lazy var pharmAppPurple = hexStringToUIColor(hex: "8884FF")
+    lazy var pharmAppRed = hexStringToUIColor(hex: "FF8D84")
+    lazy var pharmAppGrey = hexStringToUIColor(hex: "979797")
+    
+    lazy var pharmAppAnswerOne = hexStringToUIColor(hex: "BB7AE1")
+    lazy var pharmAppAnswerTwo = hexStringToUIColor(hex: "DCA480")
+    lazy var pharmAppAnswerThree = hexStringToUIColor(hex: "DA7E7E")
+    lazy var pharmAppAnswerFour = hexStringToUIColor(hex: "88D3E5")
+    
+    
+    lazy var pharmAppGameLeaderboardUserOne = hexStringToUIColor(hex: "98D8C0")
+    lazy var pharmAppGameLeaderboardUserTwo = hexStringToUIColor(hex: "C892D8")
+    lazy var pharmAppGameLeaderboardCurrentUser = hexStringToUIColor(hex: "FDFFA1")
+    lazy var pharmAppGameLeaderboardUserThree = hexStringToUIColor(hex: "A1ABFF")
+    lazy var pharmAppGameLeaderboardUserFour = hexStringToUIColor(hex: "D8939B")
+    
+
+
     func hexStringToUIColor (hex:String) -> UIColor {
         var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
         
