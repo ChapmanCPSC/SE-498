6e8ebab38aeeab915ba59b2f8cf08cdba5dff8db
Omar Sherief <omarsherief_108@Hotmail.com>
2018-03-23 19:33:48 -0700

    Update color helper to include all colors needed for app

--- iOS/MedQuiz/MedQuiz/ColorHelper.swift
+++ iOS/MedQuiz/MedQuiz/ColorHelper.swift
@@ -15,16 +15,35 @@ import UIKit
 //
 
 
-
 //use this class helper instance throught app to help with colors
 var OurColorHelper = colorHelper()
 
 //Colors
-let leaderBoardSwitchFriends = OurColorHelper.hexStringToUIColor(hex: "6FE5CB")
-let leaderBoardSwitchGlobal = OurColorHelper.hexStringToUIColor(hex: "E5896F")
-let leaderBoardHiglightUser = OurColorHelper.hexStringToUIColor(hex: "FFE483")
+//let leaderBoardSwitchFriends = OurColorHelper.hexStringToUIColor(hex: "6FE5CB")
+//let leaderBoardSwitchGlobal = OurColorHelper.hexStringToUIColor(hex: "E5896F")
+//let leaderBoardHiglightUser = OurColorHelper.hexStringToUIColor(hex: "FFE483")
+
+let pharmAppTeal = OurColorHelper.hexStringToUIColor(hex: "6FE5CB")
+let pharmAppDarkTeal = OurColorHelper.hexStringToUIColor(hex: "9AD5D2")
+let pharmAppBlue = OurColorHelper.hexStringToUIColor(hex: "439EC4")
+let pharmAppDarkBlue = OurColorHelper.hexStringToUIColor(hex: "30718D")
+let pharmAppYellow = OurColorHelper.hexStringToUIColor(hex: "FFE483")
+let pharmAppOrange = OurColorHelper.hexStringToUIColor(hex: "FFC484")
+let pharmAppPurple = OurColorHelper.hexStringToUIColor(hex: "8884FF")
+let pharmAppRed = OurColorHelper.hexStringToUIColor(hex: "FF8D84")
+let pharmAppGrey = OurColorHelper.hexStringToUIColor(hex: "979797")
+
+let pharmAppAnswerOne = OurColorHelper.hexStringToUIColor(hex: "BB7AE1")
+let pharmAppAnswerTwo = OurColorHelper.hexStringToUIColor(hex: "DCA480")
+let pharmAppAnswerThree = OurColorHelper.hexStringToUIColor(hex: "DA7E7E")
+let pharmAppAnswerFour = OurColorHelper.hexStringToUIColor(hex: "88D3E5")
 
 
+let pharmAppGameLeaderboardUserOne = OurColorHelper.hexStringToUIColor(hex: "98D8C0")
+let pharmAppGameLeaderboardUserTwo = OurColorHelper.hexStringToUIColor(hex: "C892D8")
+let pharmAppGameLeaderboardCurrentUser = OurColorHelper.hexStringToUIColor(hex: "FDFFA1")
+let pharmAppGameLeaderboardUserThree = OurColorHelper.hexStringToUIColor(hex: "A1ABFF")
+let pharmAppGameLeaderboardUserFour = OurColorHelper.hexStringToUIColor(hex: "D8939B")
 
 
 
