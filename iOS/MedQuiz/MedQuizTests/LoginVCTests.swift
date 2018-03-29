89cb490d3e3aa9a4382a7db956257eefbe3ba100
Edgar Delgado <edgardelgado@Edgars-MacBook-Pro-2.local>
2018-03-28 19:12:46 -0700

    FBSnapshot file added

---
+++ iOS/MedQuiz/MedQuizTests/LoginVCTests.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVCTests.swift
+//  MedQuizTests
+//
+//  Created by Edgar Delgado on 3/26/18.
+//  Copyright © 2018 Omar Sherief. All rights reserved.
+//
+
+import XCTest
+import FBSnapshotTestCase
+@testable import MedQuiz
+
+class LoginVCTests: FBSnapshotTestCase {
+    
+  /*  override func setUp() {
+        super.setUp()
+        recordMode = true
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+   
+    func testMainStoryBoard(){
+        //let vc = Quiz()
+        
+      //  let loginVC = OurColorHelper.pharmAppDarkBlue
+        
+        FBSnapshotVerifyView(<#T##view: UIView##UIView#>)
+    }*/
+    
+}
