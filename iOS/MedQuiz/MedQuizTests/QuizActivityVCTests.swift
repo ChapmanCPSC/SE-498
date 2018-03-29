d3b027fdf3dcb82729b5b2806f63a37b2a631919
Edgar Delgado <edgardelgado@Edgars-MacBook-Pro-2.local>
2018-03-28 19:09:46 -0700

    Added FBSnapshotTestCase pod

---
+++ iOS/MedQuiz/MedQuizTests/QuizActivityVCTests.swift
@@ -0,0 +1,26 @@
+//
+//  QuizActivityTests.swift
+//  MedQuizTests
+//
+//  Created by Edgar Delgado on 3/25/18.
+//  Copyright © 2018 Omar Sherief. All rights reserved.
+//
+
+import XCTest
+@testable import MedQuiz 
+
+class QuizActivityTests: XCTestCase {
+    
+   // let quizActivityVC = QuizActivityVC()
+    
+    /*func testHideAnswersForTime(){
+     //   let fiveSec = 5
+        
+       // var timer = Timer()
+      //  let totalTime = Timer.scheduledTimer(timeInterval: 5.0, target: self, userInfo: nil, repeats: false)
+        
+       // XCTAssertEqual(totalTime, fiveSec)
+    }*/
+   
+    
+}
---
+++ iOS/MedQuiz/MedQuizTests/UserViewTests.swift
@@ -0,0 +1,21 @@
+//
+//  UserViewTests.swift
+//  MedQuizTests
+//
+//  Created by Edgar Delgado on 3/26/18.
+//  Copyright © 2018 Omar Sherief. All rights reserved.
+//
+
+import XCTest
+@testable import MedQuiz
+
+class UserViewTests: XCTestCase {
+    
+   /* func testUpdateScore(score: Int){
+        var overallScore = 10
+        overallScore += score
+        
+        XCTAssertEqual(overallScore, score)
+    }*/
+    
+}
