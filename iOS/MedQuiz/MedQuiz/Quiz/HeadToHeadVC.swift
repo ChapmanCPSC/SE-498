ed6ca68243747c09df66c34fec7d0b5525722d77
Chad Johnson <chadjohnson@Chads-Mac.local>
2018-04-05 16:07:15 -0700

    Properly commit HeadToHeadVC progress.

---
+++ iOS/MedQuiz/MedQuiz/Quiz/HeadToHeadVC.swift
@@ -0,0 +1,73 @@
+//
+//  HeadToHeadVC.swift
+//  MedQuiz
+//
+//  Created by Chad Johnson on 3/27/18.
+//  Copyright © 2018 Omar Sherief. All rights reserved.
+//
+
+import UIKit
+
+class HeadToHeadVC: UIViewController, UITableViewDelegate, UITableViewDataSource, HeadToHeadFriendRequestViewCellDelegate {
+
+    @IBOutlet weak var friendsTableView: UITableView!
+    
+    var qiuzKey:String!
+    var friends:[Student]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //friends = user.friends!
+        
+        let friendRequestsCellNib = UINib(nibName: "HeadToHeadFriendRequestTableViewCell", bundle: nil)
+        friendsTableView.register(friendRequestsCellNib, forCellReuseIdentifier: "friendRequest_cell")
+        
+        friendsTableView.delegate = self
+        friendsTableView.dataSource = self
+        
+        friendsTableView.rowHeight = 100.0
+        friendsTableView.allowsSelection = false
+        friendsTableView.contentInset = UIEdgeInsetsMake(0, 0, 120, 0)
+        friendsTableView.separatorStyle = .none
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : HeadToHeadFriendRequestTableViewCell = friendsTableView.dequeueReusableCell(withIdentifier: "friendRequest_cell") as! HeadToHeadFriendRequestTableViewCell
+        cell.setViews(username: friends[indexPath.row].userName!, avatarImage: friends[indexPath.row].profilePic!)
+        return cell
+    }
+    
+    func requestMade(selectedFriend: Student) {
+        let quizLobbyVC = self.storyboard?.instantiateViewController(withIdentifier: "quizLobbyVC") as! QuizLobbyVC
+        quizLobbyVC.headToHeadOpponent = selectedFriend
+        quizLobbyVC.quizMode = QuizLobbyVC.QuizMode.HeadToHead
+        mainQuizVC.present(quizLobbyVC, animated: false, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
